35b44e82a47e5e4920dd116e76c438b05fe75a60
GopinathGangisetti <gopinath.gangisetti@gmail.com>
2022-11-14 11:45:49 -0600

    Updated App Header to render dynamic page links

--- Client/src/Components/app-header.js
+++ Client/src/Components/app-header.js
@@ -1,18 +1,11 @@
 import React from "react";
-import { Link } from "react-router-dom";
-export default function AppHeader() {
+export default function AppHeader(props) {
   return (
     <div className="header-main">
-    <div className="header">   
-    <h1 >Voice Chess</h1>
+      <div className="header">
+        <h1>Voice Chess</h1>
+      </div>
+      <div className="navbar">{props.children}</div>
     </div>
-     <div className="navbar">
-     <Link to="/">Home</Link>
-     <Link to="">Rankings</Link>
-     <Link to="">Watch Matches</Link>
-     <Link to="/login">Login</Link>
-     </div>
-    </div>
-   
   );
 }
