8f02a28f5fd74132e668ee3ae6ea453c2e270578
GopinathGangisetti <gopinath.gangisetti@gmail.com>
2022-11-14 19:21:07 -0600

    Updated Homepage styles

--- Client/src/Components/homepage.js
+++ Client/src/Components/homepage.js
@@ -2,33 +2,35 @@ import React from "react";
 import HomeLogo from "../images/Home_logo.png";
 import { useNavigate } from "react-router-dom";
 
-
 function HomePage() {
-
   const navigate = useNavigate();
 
-  function handleClick(){
-
+  function handleClick() {
     navigate("/login");
-
   }
 
   return (
-    <div>
-      <div className="divFlex">
-        <img src={HomeLogo} className="logo"/>
+    <>
+      <div className="flex flex-row justify-content-center">
+        <img src={HomeLogo} className="logo" />
         <br></br>
       </div>
-      <div className="divFlex">
-      <button className="btn btn-block btn-md" onClick={handleClick}>
-        PLAYER VS PLAYER
-      </button>
-      <span>OR</span>
-      <button className="btn btn-block btn-md" onClick={handleClick}>
-        PLAYER VS COMPUTER
-      </button>
+      <div className="flex flex-row justify-content-center">
+        <button
+          className="btn btn-block btn-md btn-secondary mx-3"
+          onClick={handleClick}
+        >
+          PLAYER VS PLAYER
+        </button>
+        <span> OR </span>
+        <button
+          className="btn btn-block btn-md btn-secondary mx-3"
+          onClick={handleClick}
+        >
+          PLAYER VS COMPUTER
+        </button>
       </div>
-    </div>
+    </>
   );
 }
 
